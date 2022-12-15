7bc4fd3e1d4f75d314395139b087f7ad3059edb4
Jazelle Cruz <jazellecruz22@gmail.com>
2022-12-15 11:48:35 +0800

    create delete function

--- queries/quote.js
+++ queries/quote.js
@@ -10,7 +10,7 @@ const getQuotes = async(query) => {
       let result = await Quote.find(query, {"_id" : false, "__v" : false }).sort({id : 1});
 
       if(!result.length || !Array.isArray(result)) {
-        response = new ErrorMessage(`Resource with conditions: ${(query)} does not exist.`)
+        response = new ErrorMessage(`Resource with conditions: ${query} does not exist.`)
       } else {
         response = new QuotesResponse(result);
       }
@@ -22,7 +22,7 @@ const getQuotes = async(query) => {
     return response;
 }
 
-// get quote with specified id
+// get quote with a specified id
 const getQuoteById = async(id) => {
   let response;
 
@@ -47,7 +47,7 @@ const postQuote = async(entry) => {
   let response;
   let { id, author, quote, category } = await entry
   
-  let newQuote = await new Quote({
+  let newQuote = new Quote({
     id: id,
     author: author,
     quote: quote,
@@ -66,7 +66,12 @@ const modifyQuote = async(id, entry) => {
   let response;
   try {
     let result = await Quote.updateOne({ id : id }, { $set : entry })
-    response = result
+    if (result.acknowledged) {
+      if (!result.modifiedCount) {
+        response = new ErrorMessage("Request is acknowledged but no document is modified")
+      } 
+      response = "Document is successfully modified!"
+    }
   } catch(err) {
     response = new ErrorMessage("An Error occured while performing request.", err)
   }
@@ -74,6 +79,54 @@ const modifyQuote = async(id, entry) => {
   return response;
 }
 
+// replace a whole document
+// Note: "id" MUST NOT and CANNOT be replaced
+const replaceQuote = async(id, entry) => {
+  let response;
+  let quoteId = id;
+  let { author, quote, category } = entry;
 
-module.exports = { getQuotes, getQuoteById, postQuote, modifyQuote }
+  try {
+    let result = await Quote.replaceOne({ id : quoteId },
+      {
+        id : id,
+        author: author,
+        quote: quote,
+        category: category,
+      });
+      if (result.acknowledged) {
+        if (!result.modifiedCount) {
+          response = new ErrorMessage("Request is acknowledged but no document is replaced!")
+        } 
+        response = "Document is successfully replaced!"
+      }
+  } catch(err) {
+    response = new ErrorMessage("An Error occured while performing request.", err)
+  }
+ 
+  return response;
+}
+
+// deleting a quote 
+const deleteQuote = async(id) => {
+  let response;
+  try {
+    let result = await Quote.deleteOne({ id : id });
+
+    if (result.acknowledged) {
+      if(!result.deletedCount) {
+        response = new ErrorMessage("Request is acknowledged but no document is deleted.")
+      }
+      response = `Document with id: ${id} is successfully deleted!`
+    }
+
+  } catch(err) {
+    response = new ErrorMessage("An error occured while performing deletion.", err)
+  }
+
+  return response;
+}
+
+
+module.exports = { getQuotes, getQuoteById, postQuote, modifyQuote, replaceQuote, deleteQuote }
 
