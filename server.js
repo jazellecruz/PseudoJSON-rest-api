464b5d2089bec40840a984f519f785322ae8551a
Jazelle Cruz <jazellecruz22@gmail.com>
2022-12-15 11:50:40 +0800

    change route handler name

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const quotesRoute = require("./routes/quotesRoutes.js");
+const quotes = require("./routes/quotes.js");
 
 //Database connection
 mongoose.connect("mongodb://localhost:27017/quotesDB");
 
 app.use(express.json())
-app.use("/", quotesRoute)
+app.use("/", quotes)
 
 app.listen("8000", (req, res) => {
   console.log("Magic is currently happening on port 8000.")
