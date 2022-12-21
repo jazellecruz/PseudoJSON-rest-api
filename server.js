821aa301afc4e584ee9e9619b0516464e67e05a3
Jazelle Cruz <jazellecruz22@gmail.com>
2022-12-21 17:48:23 +0800

    add route for posts

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const quotes = require("./routes/quotes.js");
+const posts = require("./routes/posts")
 
 //Database connection
 mongoose.connect("mongodb://localhost:27017/quotesDB");
 
 app.use(express.json())
-app.use("/", quotes)
+
+app.use("/quotes", quotes);
+app.use("/posts", posts);
 
 app.listen("8000", (req, res) => {
   console.log("Magic is currently happening on port 8000.")
