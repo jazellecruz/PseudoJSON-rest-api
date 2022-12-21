a4ff712e76ab5c63ff3b3c5147a17f571ac4fd00
Jazelle Cruz <jazellecruz22@gmail.com>
2022-12-21 17:49:27 +0800

    remove 'quotes' from routes

--- routes/quotes.js
+++ routes/quotes.js
@@ -4,37 +4,37 @@ const router = express.Router();
 const query = require("../queries/quote")
 
 
-router.get("/quotes", async(req, res) => {
+router.get("/", async(req, res) => {
   let response = await query.getQuotes(req.query);
   res.send(response);
 });
 
 
-router.get("/quotes/:id", async(req, res) => {
+router.get("/:id", async(req, res) => {
   let response = await query.getQuoteById(req.params.id);
   res.send(response)
 });
 
 
-router.post("/quotes", async(req, res) => {
+router.post("/", async(req, res) => {
   let response = await query.postQuote(req.body)
   res.send(response);
 });
 
 
-router.patch("/quotes/:id", async(req, res) => {
+router.patch("/:id", async(req, res) => {
   let response = await query.modifyQuote(req.params.id, req.body);
   res.send(response)
 });
 
 
-router.put("/quotes/:id", async(req, res) => {
+router.put("/:id", async(req, res) => {
   let response = await query.replaceQuote(req.params.id, req.body);
   res.send(response)
 });
 
 
-router.delete("/quotes/:id", async(req, res) => {
+router.delete("/:id", async(req, res) => {
   let response = await query.deleteQuote(req.params.id);
   res.send(response);
 });
