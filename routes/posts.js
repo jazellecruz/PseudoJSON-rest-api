a312b6baef0b6172970850d047bbc8392af47187
Jazelle Cruz <jazellecruz22@gmail.com>
2022-12-24 14:31:08 +0800

    add queries to routes

--- routes/posts.js
+++ routes/posts.js
@@ -8,23 +8,28 @@ router.get("/", async(req, res) => {
 })
 
 router.get("/:id", async(req, res) => {
-  // get post by id
+  let response = await query.getPostById(req.params.id)
+  res.send(response)
 })
 
 router.post("/", async(req, res) => {
-// add a new post 
+  let response = await query.addPost(req.body)
+  res.send(response)
 })
 
 router.patch("/:id", async(req, res) => {
-  // modify a post 
+  let response = await query.modifyPost(req.params.id, req.body)
+  res.send(response)
 })
 
 router.put("/:id", async(req, res) => {
-  // replace a post 
+  let response = await query.replacePost(req.params.id, req.body)
+  res.send(response)
 })
 
 router.delete("/:id", async(req, res) => {
-  // delete a post 
+  let response = await query.deletePost(req.params.id)
+  res.send(response)
 })
 
 module.exports = router
