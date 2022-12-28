b024f0d149fe04e950417c86d987c06926d42ba9
Jazelle Cruz <jazellecruz22@gmail.com>
2022-12-28 14:15:27 +0800

    made instance name dynamic

--- classes/apiResponse.js
+++ classes/apiResponse.js
@@ -1,12 +1,12 @@
 
-class QuotesResponse {
-  constructor(quotes, page = 1, limit = 0) {
+class ApiResponse {
+  constructor(name, quotes, page = 1, limit = 0) {
     this.message = "Successfully fullfilled request!";
-    this.quotes = quotes;
+    this[name] = quotes;
     this.total_received = quotes.length;
     this.page = page;
     this.limit = limit
   }
 }
 
-module.exports = QuotesResponse
+module.exports = ApiResponse
