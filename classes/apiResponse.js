0ceb3a6c080a548d1616191cd71e1f06a5c30255
Jazelle Cruz <jazellecruz22@gmail.com>
2022-12-29 19:03:59 +0800

    change parameter name

--- classes/apiResponse.js
+++ classes/apiResponse.js
@@ -1,8 +1,8 @@
 
 class ApiResponse {
-  constructor(quotes, name, page = 1, limit = 0) {
+  constructor(result, name, page = 1, limit = 0) {
     this.message = "Successfully fullfilled request!";
-    this[name] = quotes;
+    this[name] = result;
     this.total_received = quotes.length;
     this.page = page;
     this.limit = limit
