180f0ecd04bf36582a5b57f313adcf5a59d56c82
Jazelle Cruz <jazellecruz22@gmail.com>
2022-12-30 16:12:40 +0800

    add total count field

--- classes/apiResponse.js
+++ classes/apiResponse.js
@@ -1,8 +1,9 @@
 
 class ApiResponse {
-  constructor(data, name, page = 1, limit = 0) {
+  constructor(data, name, total, page = 1, limit = 0) {
     this.message = "Successfully fullfilled request!";
     this[name] = data;
+    this.total_found = total;
     this.total_received = data.length;
     this.page = Number(page);
     this.limit = Number(limit)
