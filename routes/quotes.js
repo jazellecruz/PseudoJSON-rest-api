d70fa937ef3c1db7c871af8e7ffa545c9e9eb3a6
Jazelle Cruz <jazellecruz22@gmail.com>
2022-12-31 13:42:31 +0800

    refactor

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,8 +1,6 @@
 
-const express = require("express");
-const router = express.Router();
+const router= require("express").Router();
 const query = require("../queries/quote")
-
 
 router.get("/", async(req, res) => {
   let response = await query.getQuotes(req.query);
