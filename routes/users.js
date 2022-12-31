8192f93bcdce57a7f9f1008a3ac7e5c82bee7e56
Jazelle Cruz <jazellecruz22@gmail.com>
2022-12-31 13:42:47 +0800

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const queries = require("../queries/user");
+
+router.get("/", async(req, res) => {
+
+})
+
+router.get("/:id", async(req, res) => {
+
+})
+
+router.post("/", async(req, res) => {
+
+})
+
+router.patch("/:id", async(req, res) => {
+
+})
+
+router.put("/:id", async(req, res) => {
+
+})
+
+router.delete("/:id", async(req, res) => {
+
+})
