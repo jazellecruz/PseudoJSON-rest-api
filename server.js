4869f017e47ec4a993a6bf7e0d4944c6dbba0287
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-01 12:28:18 +0800

    add users route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 const quotes = require("./routes/quotes.js");
 const posts = require("./routes/posts")
+const users = require("./routes/users")
 
 //Database connection
 mongoose.connect("mongodb://localhost:27017/apiDataDB");
@@ -12,6 +13,7 @@ app.use(express.json())
 
 app.use("/quotes", quotes);
 app.use("/posts", posts);
+app.use("/users", users)
 
 app.listen("8000", (req, res) => {
   console.log("Magic is currently happening on port 8000.")
