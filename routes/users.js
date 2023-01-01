b8e2f019bf65994c805e2caa5a4346a9a40288ac
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-01 12:31:10 +0800

    add queries to routes

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,39 @@
 const router = require("express").Router();
-const queries = require("../queries/user");
+const { getUsers, 
+        getUserById, 
+        addUser, 
+        modifyUser, 
+        replaceUser, 
+        deleteUser } = require("../queries/user");
 
 router.get("/", async(req, res) => {
-
-})
+  let response = await getUsers(req.query);
+  res.send(response);
+});
 
 router.get("/:id", async(req, res) => {
-
-})
+  let response = await getUserById(req.params.id);
+  res.send(response);
+});
 
 router.post("/", async(req, res) => {
-
-})
+  let response = await addUser(req.body);
+  res.send(response);
+});
 
 router.patch("/:id", async(req, res) => {
-
-})
+  let response = await modifyUser(req.params.id, req.body);
+  res.send(response);
+});
 
 router.put("/:id", async(req, res) => {
-
-})
+  let response = await replaceUser(req.params.id, req.body);
+  res.send(response);
+});
 
 router.delete("/:id", async(req, res) => {
+  let response = await deleteUser(req.params.id);
+  res.send(response);
+});
 
-})
+module.exports = router
