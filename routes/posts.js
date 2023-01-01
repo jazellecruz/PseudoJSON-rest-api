a0be4f166bf905747f44771304bc0d5c62c16872
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-01 12:30:46 +0800

    refactor

--- routes/posts.js
+++ routes/posts.js
@@ -1,34 +1,39 @@
 
 const router = require("express").Router();
-const query = require("../queries/post");
+const { getPosts, 
+        getPostById, 
+        addPost, 
+        modifyPost, 
+        replacePost, 
+        deletePost } = require("../queries/post");
 
 router.get("/", async(req, res) => {
-  let response = await query.getPosts(req.query)
+  let response = await getPosts(req.query)
   res.send(response)
 })
 
 router.get("/:id", async(req, res) => {
-  let response = await query.getPostById(req.params.id)
+  let response = await getPostById(req.params.id)
   res.send(response)
 })
 
 router.post("/", async(req, res) => {
-  let response = await query.addPost(req.body)
+  let response = await addPost(req.body)
   res.send(response)
 })
 
 router.patch("/:id", async(req, res) => {
-  let response = await query.modifyPost(req.params.id, req.body)
+  let response = await modifyPost(req.params.id, req.body)
   res.send(response)
 })
 
 router.put("/:id", async(req, res) => {
-  let response = await query.replacePost(req.params.id, req.body)
+  let response = await replacePost(req.params.id, req.body)
   res.send(response)
 })
 
 router.delete("/:id", async(req, res) => {
-  let response = await query.deletePost(req.params.id)
+  let response = await deletePost(req.params.id)
   res.send(response)
 })
 
--- routes/quotes.js
+++ routes/quotes.js
@@ -1,39 +1,44 @@
 
 const router= require("express").Router();
-const query = require("../queries/quote")
+const { getQuotes, 
+        getQuoteById, 
+        postQuote, 
+        modifyQuote, 
+        replaceQuote, 
+        deleteQuote } = require("../queries/quote")
 
 router.get("/", async(req, res) => {
-  let response = await query.getQuotes(req.query);
+  let response = await getQuotes(req.query);
   res.send(response);
 });
 
 
 router.get("/:id", async(req, res) => {
-  let response = await query.getQuoteById(req.params.id);
+  let response = await getQuoteById(req.params.id);
   res.send(response)
 });
 
 
 router.post("/", async(req, res) => {
-  let response = await query.postQuote(req.body)
+  let response = await postQuote(req.body)
   res.send(response);
 });
 
 
 router.patch("/:id", async(req, res) => {
-  let response = await query.modifyQuote(req.params.id, req.body);
+  let response = await modifyQuote(req.params.id, req.body);
   res.send(response)
 });
 
 
 router.put("/:id", async(req, res) => {
-  let response = await query.replaceQuote(req.params.id, req.body);
+  let response = await replaceQuote(req.params.id, req.body);
   res.send(response)
 });
 
 
 router.delete("/:id", async(req, res) => {
-  let response = await query.deleteQuote(req.params.id);
+  let response = await deleteQuote(req.params.id);
   res.send(response);
 });
 
