be54a0675845fa942c8cc7fa067ce6b05c62a16e
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-01 12:27:58 +0800

    initial commit

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose= require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  middleName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  birthDate: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+})
+
+module.exports = mongoose.model("User", userSchema);
