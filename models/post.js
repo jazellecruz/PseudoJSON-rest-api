466f02466014cb332c35f7e0483b071626f1402e
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-04 12:02:25 +0800

    make id field unique

--- models/post.js
+++ models/post.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const postsSchema = new mongoose.Schema({
   id: {
     type: Number,
-    required: true
+    required: true,
+    unique: true
   },
   title: {
     type: String,
--- models/quote.js
+++ models/quote.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const quotesSchema = new mongoose.Schema({
   id: {
     type: Number,
-    required: true
+    required: true,
+    unique: true
   },
   author: {
     type: String,
