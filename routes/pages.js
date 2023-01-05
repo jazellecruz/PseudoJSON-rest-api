961ecc87ab00fdeceb4398803c1093352070eead
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-05 12:55:08 +0800

    set up routes

--- routes/pages.js
+++ routes/pages.js
@@ -2,17 +2,17 @@ const router = require("express").Router();
 
 // hoome page
 router.get("/", (req, res) => {
-  res.send("This is the homepage!")
+  res.render("home");
 });
 
 // documentation page
 router.get("/docs", (req, res) => {
-  res.send("This is the documentation page!");
+  res.render("docs");
 });
 
 // about page / credits page
 router.get("/about", (req, res) => {
-  res.send("This is the about page!");
+  res.render("about");
 });
 
 module.exports = router
