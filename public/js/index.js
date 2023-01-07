84560bccb72c51e1fd5c0e390492d2744e694fc3
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-07 17:26:21 +0800

    make navbar responsive

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,10 @@
 console.log("Hello from Script!")
 
 const navBtn = document.getElementById("responsive-toggle-btn");
+const navLinks= document.getElementsByClassName("navlinks-container")[0]
 
 navBtn.addEventListener("click", () => {
   navBtn.classList.toggle("active");
-})
+  navLinks.classList.toggle("active");
+}
+)
