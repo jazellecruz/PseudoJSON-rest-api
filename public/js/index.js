e2ed146145f572f94a35ff35966baf6b3c3dbb7d
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-11 14:11:09 +0800

    remove functions used for homepage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,8 @@ console.log("Hello from jsScript!")
 
 const navBtn = document.getElementById("responsive-toggle-btn");
 const navLinks= document.getElementsByClassName("navlinks-container")[0]
-const fetchBtn = document.getElementById("fetch-btn");
-const copyBtn = document.getElementById("copy-btn");
-const codeOutput = document.getElementById("code-output")
-const exampleCode = document.getElementById("example-code").innerText;
 const copyright = document.getElementById('copyright');
+
 const currentYear = new Date().getFullYear();
 
 copyright.textContent +=  `${currentYear} PseudoJSON`;
@@ -17,29 +14,3 @@ navBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
   }
 )
-
-const fetchData = () => {
-  fetch("/quotes/1")
-  .then(res => res.json())
-  .then(json => {
-    json = json.pop();
-    json = JSON.stringify(json, null, 2)
-    codeOutput.innerHTML = hljs.highlightAuto(json).value;
-    fetchBtn.innerText = "Run Code 🏃";
-  });
-}
-
-const copyCode = () => {
-  navigator.clipboard.writeText(exampleCode);
-  copyBtn.innerText = "Copied!"
-}
-
-const handleCopyReq = () => {
-  copyBtn.innerText = "Copying..."
-  setTimeout(copyCode, 500);
-}
-
-const handleFetchReq = async() => {
-  fetchBtn.innerText = "Fetching..."
-  setTimeout(fetchData, 500);
-}
