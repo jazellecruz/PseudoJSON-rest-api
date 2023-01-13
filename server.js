d4e77b8d07337962a538f51678fd7386349f049e
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-13 23:36:30 +0800

    add routes for modifying db

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const posts = require("./src/routes/posts");
 const users = require("./src/routes/users");
 const pages = require("./src/routes/pages");
 
+// routes to modify db
+const quotesDb = require("./src/routes/quotes-db");
+const postsDb = require("./src/routes/posts-db");
+const usersDb = require("./src/routes/users-db");
+
+
 //Database connection
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -17,18 +23,29 @@ app.use(express.json());
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// for localhost
 app.use(cors({
   origin: "*",
   methods: ["GET"]
 }));
 
+
 app.use("/", pages);
 app.use("/quotes", quotes);
 app.use("/posts", posts);
 app.use("/users", users)
+
+// db routes
+app.use("/db/quotes", quotesDb);
+app.use("/db/posts", postsDb);
+app.use("/db/users", usersDb)
+
+// nonexisting routes
 app.get("*", (req, res) => {
   res.status(404).send("Page Not Found");
 })
+
 
 app.listen(8000, process.env.IP_ADDRESS || "localhost", (req, res) => {
   console.log("Magic is currently happening on port 8000.")
