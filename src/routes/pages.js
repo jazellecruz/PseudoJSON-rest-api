7cf4862153b23ea381095d57bf0d04162ed3abaf
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-16 13:45:27 +0800

    add markers for title

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -2,17 +2,17 @@ const router = require("express").Router();
 
 // hoome page
 router.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {title: "A simple REST API for dummy data"});
 });
 
 // documentation page
 router.get("/docs", (req, res) => {
-  res.render("docs");
+  res.render("docs", {title: "Documentation"});
 });
 
 // about page / credits page
 router.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {title: "About"});
 });
 
 module.exports = router
