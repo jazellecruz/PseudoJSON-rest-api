7b3a93d6724146d524c024bf848852077e2bb91c
jazzcruz <jazellecruz22@gmail.com>
2023-06-16 23:12:30 +0800

    add auth logic

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,28 +1,36 @@
 const router = require("express").Router();
 const Admin = require("../models/admin");
 const bcrypt = require("bcrypt")
+const {generateToken} = require("../utils/jwt")
 const {options} = require("../constants/constants")
 
 router.post("/login", async(req, res) => {
   let username = req.body.username;
   let password = req.body.password;
-  
+
   try{
-    let foundUser = await Admin.find({username: username}, options)
+    let foundUser = await Admin.find({username : username}, options)
 
     if(!foundUser[0]) {
       res.sendStatus(404);
       return;
     }
 
-    let isPasswordMatched = await bcrypt.compare(foundUser[0].password, password)
+    // do not remove await doing so will immediately proceed to the next line of code.
+    let isPasswordMatched = await bcrypt.compare(password, foundUser[0].password)
 
     if(!isPasswordMatched) {
       res.sendStatus(401);
       return;
     }
 
-    
+    let accessToken = generateToken(foundUser[0].username)
+
+    res.cookie("_auth", accessToken, {
+      // set to false for development only
+      httpOnly: true,
+      secure: true
+    }).send("Proceed");
 
   } catch(err){  
     res.send(err)
@@ -30,6 +38,7 @@ router.post("/login", async(req, res) => {
 });
 
 router.post("/sign-up", async(req, res) => {
+  let id = req.body.id;
   let username = req.body.username;
   let password = req.body.password;
 
@@ -37,7 +46,7 @@ router.post("/sign-up", async(req, res) => {
     let hashedPassword = await bcrypt.hash(password, saltRounds);
 
     let newAdmin = new Admin({
-      id: 1,
+      id: id,
       username: username,
       password: hashedPassword
     });
