a2624b89e539d864bb6b417a9bb76de9efe081bb
jazzcruz <jazellecruz22@gmail.com>
2023-06-19 19:18:36 +0800

    seperate user auth func

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,33 +2,23 @@ const router = require("express").Router();
 const Admin = require("../models/admin");
 const bcrypt = require("bcrypt")
 const {generateToken} = require("../utils/jwt")
-const {options} = require("../constants/constants")
 const {sanitizeInput} = require("../helpers/helpers")
+const {verifyAdminUser} = require("../queries/admin");
 
 router.post("/login", async(req, res) => {
   let username = sanitizeInput(req.body.username);
   let password = sanitizeInput(req.body.password);
 
   try{
-    let foundUser = await Admin.find({username : username}, options)
+    let isUserVerified = await verifyAdminUser(username, password)
 
-    if(!foundUser[0]) {
-      res.status(404).send("No user found.");
-      return;
-    }
-
-    // do not remove await doing so will immediately proceed to the next line of code.
-    let isPasswordMatched = await bcrypt.compare(password, foundUser[0].password)
-
-    if(!isPasswordMatched) {
-      res.sendStatus(401);
-      return;
+    if(!isUserVerified) {
+      return res.sendStatus(401);
     }
 
     let accessToken = generateToken(foundUser[0].username)
 
     res.cookie("_auth", accessToken, {
-      // set to false for development only
       httpOnly: true,
       secure: true
     }).send("Proceed");
@@ -39,9 +29,9 @@ router.post("/login", async(req, res) => {
 });
 
 router.post("/sign-up", async(req, res) => {
-  let id = req.body.id;
-  let username = req.body.username;
-  let password = req.body.password;
+  let id = sanitizeInput(req.body.id);
+  let username = sanitizeInput(req.body.username);
+  let password = sanitizeInput(req.body.password);
 
   try{ 
     let hashedPassword = await bcrypt.hash(password, saltRounds);
