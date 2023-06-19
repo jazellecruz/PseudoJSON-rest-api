600feca4b08b659644afa5d239e94606ab327a8d
jazzcruz <jazellecruz22@gmail.com>
2023-06-19 19:19:34 +0800

    add func for user auth

--- src/queries/admin.js
+++ src/queries/admin.js
@@ -1,9 +1,20 @@
+const bcrypt = require("bcrypt");
+const Admin = require("../models/admin");
+const {options} = require("../constants/constants")
 
-const findUserFromDb = (username, password) => {
+const verifyAdminUser = async(username, password) => {
   try{
-    
-  } catch(err){
+    let foundUser = await Admin.find({username : username}, options)
 
+    if(!foundUser[0]) return false;
+
+    let isPasswordMatched = await bcrypt.compare(password, foundUser[0].password)
+
+    return isPasswordMatched;
+  } catch(err){
+    console.log("An error occured in finding user from database:", err)
   }
 
 }
+
+module.exports = {verifyAdminUser}
