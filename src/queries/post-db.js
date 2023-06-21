ad9ba55587f334a2bfd292a5d6d3ca40c13fccdb
jazzcruz <jazellecruz22@gmail.com>
2023-06-21 18:43:10 +0800

    handle errors and change responses

--- src/queries/post-db.js
+++ src/queries/post-db.js
@@ -1,74 +1,87 @@
 const Post = require("../models/post");
-const ErrorMessage = require("../classes/error");
+const {ErrorMessage, APIError} = require("../classes/error");
 const { stringify, checkIfProcessed } = require("../helpers/helpers");
 
 // functions for modyfing the actual database
 
 // add new user
 const addPostOnDb = async(post) => {
-  let response;
-  let { id, title, body } = post
+  try{
+    let { id, title, body } = post;
 
-  let newPost = new Post({
-    id: id,
-    title: title,
-    body: body
-  })
+    let newPost = new Post({
+      id: id,
+      title: title,
+      body: body
+    });
 
-  await newPost.save()
-  .then(res => response = res)
-  .catch(err => response = new ErrorMessage("An error occured while performing request.", error = stringify(err.message)))
-
-  return response
+    let response = await newPost.save();
+    return response;
+  } catch(err){
+    throw (new APIError(500, "An error occured while performing request.", err));
+  }
+ 
 }
 
 // modify a single user
 const modifyPostFromDb = async(id, post) => {
-  let response
+  try{
+    let result = await Post.updateOne({ id : id }, { $set : post });
 
-  try {
-    let result = await Post.updateOne({ id : id }, { $set : post })
-    response = checkIfProcessed(result.acknowledged, result.modifiedCount, "PATCH")
+    let response = {
+      isAcknowledged: !!result.acknowledged,
+      isSuccessful: !!result.modifiedCount,
+      modifiedCount: result.modifiedCount,
+    }
+
+    return response;
   } catch(err) {
-    response = new ErrorMessage("An error occured while performing request.", error = stringify(err.message))
+    throw (new APIError(500, "An error occured while performing request.", err));
   }
-
-  return response
 }
 
 // replace a post 
 // NOTE: I DO NOT ADVISE TO REPLACE A WHOLE DOCUMENT
 // I SUGGEST TO CREATE A NEW ONE INSTEAD TO AVOID DUPLICATION OF ID
 const replacePostFromDb = async(id, post) => {
-  let response
-  let postId = id
-  let { newId, title, body } = post
-
   try {
+    let postId = id
+    let { newId, title, body } = post
+    
     let result = await Post.replaceOne({ id: postId }, {
       id: newId,
       title: title,
       body: body
-    })
-    response = checkIfProcessed(result.acknowledged, result.modifiedCount, "PUT")
+    });
+
+    let response = {
+      isAcknowledged: !!result.acknowledged,
+      isSuccessful: !!result.modifiedCount,
+      modifiedCount: result.modifiedCount,
+    }
+
+    return response;
   } catch(err) {
-    response = new ErrorMessage("An error occured while performing request.", error = stringify(err.message))
+    throw (new APIError(500, "An error occured while performing request.", err));
   }
 
-  return response
 }
 
 //delete a single user
 const deletePostFromDb = async(id) => {
-  let response 
-
   try {
     let result = await Post.deleteOne({ id : id })
-    response = checkIfProcessed(result.acknowledged, result.deletedCount, "DELETE")
+
+    let response = {
+      isAcknowledged: !!result.acknowledged,
+      isSuccessful: !!result.deletedCount,
+      deletedCount: result.deletedCount,
+    }
+
+    return response;
   } catch(err) {
-    response = new ErrorMessage("An error occured while performing request.", error = stringify(err.message))
+    throw (new APIError(500, "An error occured while performing request.", err));
   }
-  return response
 }
 
 module.exports = { addPostOnDb, modifyPostFromDb, replacePostFromDb, deletePostFromDb }
