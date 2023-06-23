10a2c534213f39e73b20058402a5025fd40eed5b
jazzcruz <jazellecruz22@gmail.com>
2023-06-23 22:10:05 +0800

    replace err classes with client and server

--- src/classes/error.js
+++ src/classes/error.js
@@ -1,20 +1,17 @@
+class ClientError extends Error {
+  constructor(type, httpCode, clientMessage, err){
+    super(err);
 
-class ErrorMessage{
-  constructor(message, error = undefined , code = undefined) {
-    this.status_code = code;
-    this.error =  error;
-    this.message = message;
-  }
-}
-
-class APIError extends Error {
-  constructor(httpCode, clientMessage, err){
-    super(err)
-
-    // this.type = type
+    this.type = type;
     this.httpCode = httpCode;
-    this.clientMessage = clientMessage;
-    // this.details = details
+    this.clientMessage = clientMessage
   }
 }
-module.exports = {ErrorMessage, APIError}
+
+class ServerError extends Error {
+  constructor(err){
+    super(err);
+  }
+}
+
+module.exports = {ClientError, ServerError}
