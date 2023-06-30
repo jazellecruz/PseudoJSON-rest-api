e8d7128e72e34809af908d0ad3aa1030690384a0
jazzcruz <jazellecruz22@gmail.com>
2023-06-30 20:02:52 +0800

    handle db disconnections/reconnections

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const connectToDB = async() => {
+const MAX_RETRY_ATTEMPTS = 5;
+
+const connectToDB = async(retryCount = 0) => {
   try{
    await mongoose.connect(process.env.MONGODB_DEV, {
       useNewUrlParser: true,
@@ -9,10 +11,14 @@ const connectToDB = async() => {
     
     console.log("Database connected successfully...");
   } catch(err) {
-    // if (err.code === "ECONNREFUSED" || err.code === "ETIMEOUT") {
-    //   setTimeout(connectToDB(), 10000);
-    //   return;
-    // }
+
+    // attempt to reconnect to the database for only 5 times
+    if(retryCount < MAX_RETRY_ATTEMPTS && (err.code === "ECONNREFUSED" || err.code === "ETIMEOUT")){
+      const delay = (retryCount + 1) * 5 * 1000;
+      setTimeout(() => connectToDB(retryCount + 1), delay);
+      console.log(`Failed to connect to database. Retrying in ${delay / 1000}s`);
+      return;
+    }
 
     console.log("Failed to connect to database...");
     console.log(err);
