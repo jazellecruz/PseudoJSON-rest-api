02bfba68cb6476a221859e9e33deb077f7181688
jazzcruz <jazellecruz22@gmail.com>
2023-07-03 12:32:05 +0800

    add try/catch for err

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -1,18 +1,31 @@
 const router = require("express").Router();
+const {ServerError} = require("../classes/error");
 
 // hoome page
-router.get("/", (req, res) => {
-  res.render("home", {title: "A simple REST API for dummy data"});
+router.get("/", (req, res, next) => {
+  try{
+    res.render("home", {title: "A simple REST API for dummy data"});
+  } catch(err) {
+    next(new ServerError(err));
+  }
 });
 
 // documentation page
-router.get("/docs", (req, res) => {
-  res.render("docs", {title: "Documentation"});
+router.get("/docs", (req, res, next) => {
+  try{
+    res.render("docs", {title: "Documentation"});
+  } catch(err) {
+    next(new ServerError(err));
+  }
 });
 
 // about page / credits page
-router.get("/about", (req, res) => {
-  res.render("about", {title: "About"});
+router.get("/about", (req, res, next) => {
+  try{
+    res.render("about", {title: "About"});
+  } catch(err) {
+    next(new ServerError(err));
+  }
 });
 
 module.exports = router
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken");
+const {ServerError} = require("../classes/error")
 
 const generateToken = (user) => {
   try{
     let accessToken = jwt.sign({user: user}, process.env.ACCESS_SECRET_KEY, { expiresIn: '6h' })
     return accessToken;
   } catch(err) {
-    throw err;
+    throw new ServerError(err);
   }
 }
 
